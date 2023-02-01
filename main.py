9e94454db959a69442fb42e114d4f4e5760c6162
rue1-suzuki <suzuki.ryuichi@formzu.co.jp>
2023-02-01 15:28:44 +0900

    1st commit

---
+++ main.py
@@ -0,0 +1,9 @@
+def is_prime(n: int) -> bool:
+    if n < 2:
+        return False
+
+    for index in range(2, n):
+        if n % index == 0:
+            return False
+
+    return True
---
+++ test_prime.py
@@ -0,0 +1,22 @@
+from main import is_prime
+
+MAX = 50
+MIN = -MAX
+PRIME_LIST = [
+    2, 3, 5, 7,
+    11, 13, 17, 19,
+    23, 29,
+    31, 37,
+    41, 43, 47,
+]
+
+
+def test_is_prime():
+    for index in range(MIN, 1 + MAX):
+
+        is_exist_in_prime_list = list(filter(
+            lambda x: x == index,
+            PRIME_LIST,
+        )).__len__() > 0
+
+        assert is_exist_in_prime_list == is_prime(index)
