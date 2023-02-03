3043b67278edccdc1b6ff417e5cc53a2531c36ca
rue1-suzuki <suzuki.ryuichi@formzu.co.jp>
2023-02-03 13:37:21 +0900

    1st commit

--- test_prime.py
+++ test_prime.py
@@ -1,18 +1,18 @@
 from main import is_prime
 
 MAX = 30
-MIN = -  MAX
+MIN = -MAX
 PRIME_LIST = [
     2, 3, 5, 7,
-                11, 13, 17, 19,
-    23,  29,
-    31,  37,
+    11, 13, 17, 19,
+    23, 29,
+    31, 37,
     41, 43, 47,
 ]
 
 
 def test_is_prime():
-    for index  in range(MIN, 1 + MAX):
+    for index in range(MIN, 1 + MAX):
 
         is_exist_in_prime_list = list(filter(
             lambda x: x == index,
